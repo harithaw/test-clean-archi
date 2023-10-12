38cb86368a7e5f3b0342dc3c6f3534a47d15b1e6
Haritha Weerathunga <122088594+harithaw@users.noreply.github.com>
2023-10-12 09:53:46 +0530

    added keycloak implementation

--- server.js
+++ server.js
@@ -14,6 +14,23 @@ const db2 = new mongodbUser();
 const app = express();
 app.use(express.json());
 
+// KEYCLOAK IMPLEMENTATION
+
+// const Keycloak = require('keycloak-connect');
+// const session = require('express-session');
+
+// var memoryStore = new session.MemoryStore();                       
+// var keycloak = new Keycloak({ store: memoryStore });
+// //session                       
+// app.use(session({
+//     secret:'BeALongSecret',                         
+//     resave: false,                         
+//     saveUninitialized: true,                         
+//     store: memoryStore                       
+// }));
+
+// app.use(keycloak.middleware());
+
 app.use("/api", UserRouter(db1))
 
 app.listen(3000, () => {

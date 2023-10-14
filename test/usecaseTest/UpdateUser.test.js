ab9bf7985ff181bb5b488679a93472acf6caef53
Haritha Weerathunga <122088594+harithaw@users.noreply.github.com>
2023-10-14 13:25:59 +0530

    added userupdate usercase

--- test/usecaseTest/UpdateUser.test.js
+++ test/usecaseTest/UpdateUser.test.js
@@ -2,10 +2,16 @@ const UpdateUser = require("../../usecases/user/UpdateUser");
 const inMemoryUserDB = require("../../db/repository/inMemoryUserDB");
 
 describe("Update User", () =>{
+    let db1;
+    const user2 = {id: 2, username : "editTestUser02", password : "editTestPassword2", firstname : "testWomen", dob : "1998"}
 
+    beforeEach(() => {
+        db1 = new inMemoryUserDB();
+        db1.init();
+    })
 
     test("should update a user", async () => {
-
+        expect( await UpdateUser(user2.id, {username:"editTestUser02", password : "editTestPassword2"}, db1)).toEqual(user2);
     })
 
 })
--- usecases/user/UpdateUser.js
+++ usecases/user/UpdateUser.js
@@ -1,5 +1,24 @@
-async function UpdateUser(){
+async function UpdateUser(id, data, userRepository){
 
+    const user = await userRepository.getById(id);
+
+    if (data.hasOwnProperty('username')) {
+        user.username = data.username;
+    }
+
+    if (data.hasOwnProperty('password')) {
+        user.password = data.password;
+    }
+
+    if (data.hasOwnProperty('firstname')) {
+        user.firstname = data.firstname;
+    }
+
+    if (data.hasOwnProperty('dob')) {
+        user.dob = data.dob;
+    }
+
+    return user;
 }
 
 module.exports = UpdateUser;
